750ec5e64a04fb31f216785b09f1a7b9a57c898c
Isabila2 <125681801+Isabila2@users.noreply.github.com>
2023-05-05 10:40:16 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,4 +1,3 @@
-
 var idade;
 var resultado;
 var nome;
@@ -8,7 +7,10 @@ function fase(){
     nome = document.getElementById("nome").value;
      resultado = document.getElementById("resultado");
 
-    if (idade <= 11) {
+    if(idade <= 0){
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "</br>Ops... </br> Parece que você adicionou um valor incorreto...";
+    } else if (idade <= 11) {
         document.getElementById("resultado").innerHTML = "<br/> Parabéns "+ nome + "!!<br/></br>" +  " Você tem " + idade + " anos!" + "<br/> Você é uma criança! Aproveite a infância!";
    } else if (idade > 11 && idade <=20){
        resultado = document.getElementById("resultado");
@@ -16,10 +18,19 @@ function fase(){
     } else if ( idade > 20 && idade <=64){
         resultado = document.getElementById("resultado");
        resultado.innerHTML = "<br/> Parabéns "+ nome + "!!<br/></br>" +  " Você tem " + idade + " anos!" + "<br/> Você é um adulto!";
-   } else if (idade >= 65){
+   } else if (idade >= 65 && idade <= 110){
       resultado = document.getElementById("resultado");
      resultado.innerHTML = "<br/> Parabéns "+ nome + "!!<br/></br>" +  " Você tem " + idade + " anos!" + "<br/> Você é um idoso! Já possui bastante experiência!";
+   } else if (idade >= 111 && idade <=200){
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = "</br> Nossa " + nome +  "!!!! </br> Você está tão  velho(a) assim?? </br> Parabéns, ein!";
+   } else if (idade >=201 && idade <= 1000){
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = "</br> Ok... </br> Ai já é exagero... "
+   } else if (idade >=1001){
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = "</br> Adicione a sua idade verdadeira!"
    }
 
-   
+
 }
