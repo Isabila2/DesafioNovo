c086abc9cb8e95e2fa3c22f135107e135226fbb6
Isabila2 <125681801+Isabila2@users.noreply.github.com>
2023-05-05 08:13:43 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+
+var idade;
+var resultado;
+var nome;
+
+function fase(){
+    idade = document.getElementById("idade").value;
+    nome = document.getElementById("nome").value;
+     resultado = document.getElementById("resultado");
+
+    if (idade <= 11) {
+        document.getElementById("resultado").innerHTML = "<br/> Parabéns "+ nome + "!!<br/></br>" +  " Você tem " + idade + " anos!" + "<br/> Você é uma criança! Aproveite a infância!";
+   } else if (idade > 11 && idade <=20){
+       resultado = document.getElementById("resultado");
+       resultado.innerHTML = "<br/> Parabéns "+ nome + "!!<br/></br>" +  " Você tem " + idade + " anos!" + "<br/> Você é um adolescente! Está ganhando novas responsabilidades!";
+    } else if ( idade > 20 && idade <=65){
+        resultado = document.getElementById("resultado");
+       resultado.innerHTML = "<br/> Parabéns "+ nome + "!!<br/></br>" +  " Você tem " + idade + " anos!" + "<br/> Você é um adulto! Precisa arrumar um emprego!";
+   } else if (idade > 65){
+      resultado = document.getElementById("resultado");
+     resultado.innerHTML = "<br/> Parabéns "+ nome + "!!<br/></br>" +  " Você tem " + idade + " anos!" +"<br/> Seus resultado foi:" + idade + "<br/> Você é um idoso! Já possui bastante experiência!";
+   }
+
+   
+}
